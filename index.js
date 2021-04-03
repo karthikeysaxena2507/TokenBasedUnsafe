216bf111a6a262ff881019478afddf78809ceeb1
Karthikey <karthikeysaxena@gmail.com>
2021-04-04 00:14:25 +0530

    changed

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const app = express();
 
 // app.use(cors());
 app.use(cors({
-    origin: "http://localhost:3001",
-    optionsSuccessStatus: 200,
+    origin: true,
     credentials: true
   }));
 app.use(express.json());
